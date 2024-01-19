ec761eb04aa4e82a915ed7b7f592d1cd0ad03813
shumancheng <Shumancheng0119@gmail.com>
2024-01-19 22:24:02 +0100

    Update

---
+++ HackoverFlow/frontend/src/app/core/services/kitchen.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class KitchenService {
+  baseUrl = "/kitchenplan";
+
+  constructor(private http: HttpClient) { }
+
+  create(plan :any) : Observable<any>{
+    return this.http.post(environment.SERVER_URL + this.baseUrl, plan,
+  {headers: {
+    "Access-Control-Allow-Origin": "*"
+}});}
+}
