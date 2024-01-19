f9c5311c234a1d9b82f98199a9be1bc1512e584b
shumancheng <Shumancheng0119@gmail.com>
2024-01-19 20:56:01 +0100

    SubmitForm

--- HackoverFlow/frontend/src/app/pages/kitchen-menu/kitchen-menu.component.ts
+++ HackoverFlow/frontend/src/app/pages/kitchen-menu/kitchen-menu.component.ts
@@ -6,13 +6,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./kitchen-menu.component.scss']
 })
 export class KitchenMenuComponent {
-  mealOptions: string[] = ['Meat Option', 'Vegetarian Option', 'Other Option']; // Add other meal options as needed
-
-  meals: any = {
-    Monday: { 'Meat Option': '', 'Vegetarian Option': '', 'Other Option': '' },
-    Tuesday: { 'Meat Option': '', 'Vegetarian Option': '', 'Other Option': '' },
-    Wednesday: { 'Meat Option': '', 'Vegetarian Option': '', 'Other Option': '' },
-    Thursday: { 'Meat Option': '', 'Vegetarian Option': '', 'Other Option': '' },
-    Friday: { 'Meat Option': '', 'Vegetarian Option': '', 'Other Option': '' }
+  menu = {
+    monday: {
+      mealtype: {
+        meat: '',
+        vegetarian: '',
+        vegan: ''
+      }
+    },
+    tuesday: {
+      mealtype: {
+        meat: '',
+        vegetarian: '',
+        vegan: ''
+      }
+    },
+    wednesday: {
+      mealtype: {
+        meat: '',
+        vegetarian: '',
+        vegan: ''
+      }
+    },
+    thursday: {
+      mealtype: {
+        meat: '',
+        vegetarian: '',
+        vegan: ''
+      }
+    },
+    friday: {
+      mealtype: {
+        meat: '',
+        vegetarian: '',
+        vegan: ''
+      }
+    }
   };
+  
+
+  weeklySalad: string = '';
+
+  onSubmit(menuData: any) {
+    // Handle form submission logic here
+    console.log('Form submitted with data:', menuData);
+    // You can perform additional logic, like sending the data to a server
+  }
 }
